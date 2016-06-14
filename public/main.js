9926b0f7846e7a2193bf8bb02685e70522296f1d
Mark Easterling <marke.easterling@gmail.com>
2016-06-14 11:17:04 -0500

    goal data being stored in firebase

--- public/main.js
+++ public/main.js
@@ -7,19 +7,19 @@ angular.module("life", [])
     storageBucket: "life-tracker-e5c81.appspot.com",
   })))
 
-  .controller("MainCtrl", function(){
+  .controller("MainCtrl", function($http){
   	main = this;
   	main.heading = "Lifetracker";
     main.goals=[]
 
   	main.submitGoal = function(){
-  		console.log(main.goalTitle)
-  		console.log(main.goalDescription)
-  		console.log(main.goalPoints)
-  		console.log(main.goalLength)
-      console.log(main.goalImportance)
-      console.log(main.goalCategory)
-      main.goals.push({
+  		// console.log(main.goalTitle)
+  		// console.log(main.goalDescription)
+  		// console.log(main.goalPoints)
+  		// console.log(main.goalLength)
+    //   console.log(main.goalImportance)
+    //   console.log(main.goalCategory)
+      firebase.database().ref('/goals/').push({
         "title": main.goalTitle,
         "description": main.goalDescription,
         "points": main.goalPoints,
@@ -27,7 +27,16 @@ angular.module("life", [])
         "importance": main.goalImportance,
         "category": main.goalCategory
       })
-      console.log(main.goals)
+
+      // main.goals.push({
+      //   "title": main.goalTitle,
+      //   "description": main.goalDescription,
+      //   "points": main.goalPoints,
+      //   "length": main.goalLength,
+      //   "importance": main.goalImportance,
+      //   "category": main.goalCategory
+      // })
+      // console.log(main.goals)
   	};
 
 
