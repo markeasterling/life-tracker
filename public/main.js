017e58bc8edcc6a4ee55f9be70c33df758b1fa08
Mark Easterling <marke.easterling@gmail.com>
2016-06-15 12:25:52 -0500

    date started and date to end stored in firebase

--- public/main.js
+++ public/main.js
@@ -22,16 +22,25 @@ angular.module("life", ['angular.filter'])
       ))
     )
 
-    main.completeGoal = function (key) {
-      console.log(key)
-      return firebase.database().ref(`/goals/${key}`)
-        .transaction(goal => {
-          goal.complete = true
-          return goal
-        })
-    }
+  main.completeGoal = function (key) {
+    console.log(key)
+    return firebase.database().ref(`/goals/${key}`)
+      .transaction(goal => {
+        goal.complete = true
+        return goal
+      })
+  }
+
+  main.showGoal = function (key) {
+    console.log(key)
+    let current=main.goals[key]
+    console.log(current)
+    goalDisplay = `<div>Title: ${current.title}</div>`
+  }
 
     main.submitGoal = function (){
+      let start = Date.now()
+      let end = start + main.goalLength * 86400000
 
       firebase.database().ref('/goals/').push({
         "title": main.goalTitle,
@@ -40,7 +49,9 @@ angular.module("life", ['angular.filter'])
         "length": main.goalLength,
         "importance": main.goalImportance,
         "category": main.goalCategory,
-        "complete": false
+        "complete": false,
+        "dateStarted": start,
+        "dateEnded": end
       })
 
   	};
