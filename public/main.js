12ea86d1ddc496560f928cbdd8fb708b47bb6e91
Mark Easterling <marke.easterling@gmail.com>
2016-06-17 10:11:42 -0500

    retreiving time from firebase

--- public/main.js
+++ public/main.js
@@ -12,28 +12,28 @@ angular.module("life", ['angular.filter'])
     main = this;
     main.heading = "Lifetracker";
     //main.currentUserId= "zS0MHocQRcWgB14PmyH6VvP085I2"
-    let currentUserId = "zS0MHocQRcWgB14PmyH6VvP085I2";
+    //let currentUserId;
     //$scope.currentUserId = userId
 
 
   const setCurrentTime = () => {
     firebase.database().ref('/time/')
       .update({"currentTime": Date.now()})
-      console.log(currentUserId)
+      //console.log(currentUserId)
   };
   setCurrentTime();
 
 //NOT SURE ABOUT THIS PART/////
-  // const updateTime = snapshot => (
-  //   $timeout().then(() => (
-  //     main.time = Object.assign(
-  //       {},
-  //       main.time,
-  //       { [snapshot.key]: snapshot.val()}
-  //     )
-  //   )).then(console.log(main.time.currentTime))
-  // )
-///////////////////////////////
+  const updateTime = snapshot => (
+    $timeout().then(() => (
+      main.time = Object.assign(
+        {},
+        main.time,
+        { [snapshot.key]: snapshot.val() }
+      )
+    ))
+  )
+/////////////////////////////
 
   const updateGoals = (snapshot) => (
     $timeout().then(() => (
@@ -75,11 +75,10 @@ angular.module("life", ['angular.filter'])
       "complete": false,
       "dateStarted": start,
       "dateEnded": end,
-      "userId": userId
+      "userId": main.currentUserId
     })
-      console.log(userId)
-      console.log(currentUserId)
       setCurrentTime();
+      console.log(main.time)
 	};
 
   main.login = function (email, password) {
@@ -88,21 +87,38 @@ angular.module("life", ['angular.filter'])
     firebase.auth().signInWithEmailAndPassword(email, password)
   };
 
+  main.reset = function () {
+    firebase.database().ref('/time/').once('value').then(function(snapshot) {
+      main.time = snapshot.val()
+      console.log("main.time:", main.time)
+
+      if (main.time.currentTime > main.time.inceptionTime) {
+        console.log(true)
+      } else {
+        console.log(false)
+      }
+
+    })
+  }
 
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         // console.log("user id", user.uid)
         // console.log(user.getToken())
-        userId = user.uid
-        console.log(userId)
-        main.currentUserId = userId
-        console.log(currentUserId)
+        console.log(user.email)
+        main.currentUserId = user.uid
+        main.currentUserEmail= user.email
+
+
     }
   })
+
+
+
 
     firebase.database().ref('/goals/').on('child_added', updateGoals)
     firebase.database().ref('/goals/').on('child_changed', updateGoals)
    /////////////////NOT SURE ABOUT THIS////////////
-    //firebase.database().ref('/time/').on('child_changed', updateTime)
+    firebase.database().ref('/time/').on('child_changed', updateTime)
    /////////////////////////////////////////////////
   });
