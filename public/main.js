bd1ff1c7fe6a724d46f7268ce5d177243e6a4c08
Mark Easterling <marke.easterling@gmail.com>
2017-02-02 21:11:04 -0500

    added semicolons

--- public/main.js
+++ public/main.js
@@ -14,11 +14,11 @@ var app = angular
     main.heading = "Lifetracker";
     main.loginHeader = "login";
 
-
   const setCurrentTime = () => {
     firebase.database().ref('/time/')
       .update({"currentTime": Date.now()})
-  };
+  }
+
   setCurrentTime();
 
   const updateTime = snapshot => (
@@ -39,23 +39,24 @@ var app = angular
         { [snapshot.key]: snapshot.val() }
       )
     ))
-  );
+  )
 
   main.completeGoal = function (key) {
     return firebase.database().ref(`/goals/${key}`)
       .transaction(goal => {
-        goal.complete = true
-        goal.record.push(true)
+        goal.complete = true;
+        goal.record.push(true);
         return goal
       })
   };
 
   main.deleteGoal = function (key) {
-    firebase.database().ref(`/goals/${key}`).remove()
+    firebase.database().ref(`/goals/${key}`).remove();
   }
 
   main.submitGoal = function () {
-    firebase.database().ref('/goals/').push({
+    firebase.database().ref('/goals/').push(
+      {
       "title": main.goalTitle,
       "description": main.goalDescription,
       "importance": main.goalImportance,
@@ -65,21 +66,20 @@ var app = angular
       "record": ["true"],
       "percentComplete": 0,
       "userId": main.currentUserId
-    })
+      }
+    )
+      main.goalTitle = "";
+      main.goalDescription = "";
+      main.goalImportance = "";
+      main.goalCategory = "";
+      main.goalFrequency = "";
       setCurrentTime();
-      main.goalTitle = ""
-      main.goalDescription = ""
-      main.goalImportance = ""
-      main.goalCategory = ""
-      main.goalFrequency = ""
-	};
+	}
 
   main.login = function (email, password) {
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then(() => {main.switchToMain()})
-
-
-  };
+  }
 
 	main.register = function (email, password) {
 		firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -88,29 +88,27 @@ var app = angular
 	}
 
   main.logout = function () {
-    firebase.auth().signOut().then(function(){console.log("sign out success")
+    firebase.auth().signOut().then(function(){console.log("sign out success");
     })
-    main.currentUserId = undefined
-    $location.path('/')
+    main.currentUserId = undefined;
+    $location.path('/');
   }
 
   main.switchToMain = function () {
     // if (main.currentUserId != undefined) {
-      $timeout().then(() => {$location.path("/main")})
+      $timeout().then(() => {$location.path("/main");})
     // }
   }
 
   main.reset = function () {
     firebase.database().ref('/time/').once('value').then(function(snapshot) {
-      main.time = snapshot.val()
-      //console.log("main.time:", main.time)
+      main.time = snapshot.val();
 
       if (main.time.currentTime > main.time.inceptionTime) {
-        //console.log(true)
-        let goals = main.goals
+        let goals = main.goals;
         firebase.database().ref(`/time/`)
               .transaction(time => {
-                time.inceptionTime = time.inceptionTime + 86400000 //ms in a day
+                time.inceptionTime = time.inceptionTime + 86400000; //86400000ms= 24hrs
                 return time
               })
 
@@ -125,8 +123,8 @@ var app = angular
                 let trues = goal.record.filter(numberTrue)
 
                 //goal.record.push(goal.complete)
-                goal.complete = false
-                goal.percentComplete = Math.round((trues.length / (goal.record.length - 1)) * 100)
+                goal.complete = false;
+                goal.percentComplete = Math.round((trues.length / (goal.record.length - 1)) * 100);
                 return goal
               })
                 //console.log(goal.record)
@@ -136,11 +134,11 @@ var app = angular
                 function numberTrue(value){
                   return value == true
                 }
-                let trues = goal.record.filter(numberTrue)
+                let trues = goal.record.filter(numberTrue);
 
-                goal.record.push(goal.complete)
-                goal.complete = false
-                goal.percentComplete = Math.round((trues.length / (goal.record.length - 1)) * 100)
+                goal.record.push(goal.complete);
+                goal.complete = false;
+                goal.percentComplete = Math.round((trues.length / (goal.record.length - 1)) * 100);
                 return goal
               })
           }
@@ -154,26 +152,26 @@ var app = angular
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         // console.log(user.email)
-        main.currentUserId = user.uid
-        main.currentUserEmail= user.email
+        main.currentUserId = user.uid;
+        main.currentUserEmail= user.email;
     }
   })
 
   main.loadCharts = function() {
     main.loadAllGoals = function () {
-      let goals = main.goals
+      let goals = main.goals;
       let allGoals = { "goals": []}
       for ( obj in goals ) {
         if ( goals[obj].userId == main.currentUserId ) {
           let goalobj = {
             "label": goals[obj].title,
             "n": goals[obj].percentComplete
-          }
-          allGoals.goals.push(goalobj)
+          };
+          allGoals.goals.push(goalobj);
           // console.log(goalobj)
           //console.log(allGoals)
         } else {
-          console.log(false)
+          console.log(false);
         }
       }
 
@@ -191,55 +189,55 @@ var app = angular
       let physicalArray = [];
       let workArray = [];
       let personalArray = [];
-      let physicalObj
-      let workObj
-      let personalObj
+      let physicalObj;
+      let workObj;
+      let personalObj;
 
 
       for (obj in goals) {
         if (goals[obj].category == "physical" && goals[obj].userId == main.currentUserId) {
           physicalArray.push(goals[obj].percentComplete)
           let percentArray = physicalArray.reduce((a, b) => a + b, 0);
-          let avg = percentArray/physicalArray.length
+          let avg = percentArray/physicalArray.length;
           physicalObj = {
             "label": "physical",
             "n": avg
-          }
+          };
         } else if (goals[obj].category == "work" && goals[obj].userId == main.currentUserId) {
-          workArray.push(goals[obj].percentComplete)
+          workArray.push(goals[obj].percentComplete);
           let percentArray = workArray.reduce((a, b) => a + b, 0);
-          let avg = percentArray/workArray.length
+          let avg = percentArray/workArray.length;
           workObj = {
             "label": "work",
             "n": avg
-          }
+          };
         } else if (goals[obj].category == "personal" && goals[obj].userId == main.currentUserId) {
-          personalArray.push(goals[obj].percentComplete)
+          personalArray.push(goals[obj].percentComplete);
           let percentArray = personalArray.reduce((a, b) => a + b, 0);
-          let avg = percentArray/personalArray.length
+          let avg = percentArray/personalArray.length;
           personalObj = {
             "label": "personal",
             "n": avg
-          }
+          };
         }
       }
 
 			if (physicalObj){
-				catObj.goals.push(physicalObj)
+				catObj.goals.push(physicalObj);
 			} else {
-				console.log("physicalObj does not exist")
+				console.log("physicalObj does not exist");
 			}
 
 			if (workObj){
-				catObj.goals.push(workObj)
+				catObj.goals.push(workObj);
 			} else {
-				console.log("workObj does not exist")
+				console.log("workObj does not exist");
 			}
 
 			if (personalObj){
-				catObj.goals.push(personalObj)
+				catObj.goals.push(personalObj);
 			} else {
-				console.log("personalObj does not exist")
+				console.log("personalObj does not exist");
 			}
 
 
@@ -257,55 +255,54 @@ var app = angular
       let lowArray = [];
       let normalArray = [];
       let highArray = [];
-      let lowObj
-      let normalObj
-      let highObj
-
+      let lowObj;
+      let normalObj;
+      let highObj;
 
       for (obj in goals) {
         if (goals[obj].importance == "low" && goals[obj].userId == main.currentUserId) {
-          lowArray.push(goals[obj].percentComplete)
+          lowArray.push(goals[obj].percentComplete);
           let percentArray = lowArray.reduce((a, b) => a + b, 0);
-          let avg = percentArray/lowArray.length
+          let avg = percentArray/lowArray.length;
           lowObj = {
             "label": "low",
             "n": avg
-          }
+          };
         } else if (goals[obj].importance == "normal" && goals[obj].userId == main.currentUserId) {
           normalArray.push(goals[obj].percentComplete)
           let percentArray = normalArray.reduce((a, b) => a + b, 0);
-          let avg = percentArray/normalArray.length
+          let avg = percentArray/normalArray.length;
           normalObj = {
             "label": "normal",
             "n": avg
-          }
+          };
         } else if (goals[obj].importance == "high" && goals[obj].userId == main.currentUserId) {
-          highArray.push(goals[obj].percentComplete)
+          highArray.push(goals[obj].percentComplete);
           let percentArray = highArray.reduce((a, b) => a + b, 0);
-          let avg = percentArray/highArray.length
+          let avg = percentArray/highArray.length;
           highObj = {
             "label": "high",
             "n": avg
-          }
+          };
         }
       }
 
 			if (lowObj){
-				priorityObj.goals.push(lowObj)
+				priorityObj.goals.push(lowObj);
 			} else {
-				console.log("lowObj does not exist")
+				console.log("lowObj does not exist");
 			}
 
 			if (normalObj){
-				priorityObj.goals.push(normalObj)
+				priorityObj.goals.push(normalObj);
 			} else {
-				console.log("normalObj does not exist")
+				console.log("normalObj does not exist");
 			}
 
 			if (highObj){
-				priorityObj.goals.push(highObj)
+				priorityObj.goals.push(highObj);
 			} else {
-				console.log("highObj does not exist")
+				console.log("highObj does not exist");
 			}
 			// priorityObj.goals.push(lowObj)
       // priorityObj.goals.push(normalObj)
@@ -319,15 +316,15 @@ var app = angular
       .style("width", function(d) { return d.n * 4 + "px"; })
       .text(function(d) { return `${d.label} priority- ${Math.floor(d.n)}%` });
     }
-    main.loadAllGoals()
-    main.loadPriorityGoals()
-    main.loadCategorizedGoals()
+    main.loadAllGoals();
+    main.loadPriorityGoals();
+    main.loadCategorizedGoals();
   }
 
 
 
-  firebase.database().ref('/goals/').on('child_added', updateGoals)
-  firebase.database().ref('/goals/').on('child_changed', updateGoals)
-  firebase.database().ref('/goals/').on('child_removed', updateGoals)
-  firebase.database().ref('/time/').on('child_changed', updateTime)
+  firebase.database().ref('/goals/').on('child_added', updateGoals);
+  firebase.database().ref('/goals/').on('child_changed', updateGoals);
+  firebase.database().ref('/goals/').on('child_removed', updateGoals);
+  firebase.database().ref('/time/').on('child_changed', updateTime);
   });
